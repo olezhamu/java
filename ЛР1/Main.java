4c912fb9cba65e71911bbe51d939381de30e448d
olezhamu <mushkevich.oleg@gmail.com>
2025-10-01 11:03:23 +0300

    Add files via upload

--- ЛР1/Main.java
+++ ЛР1/Main.java
@@ -15,26 +15,35 @@ public class Main{
             switch ((int)s[i]) {
                 case 17:
                     for (int j = 0; j<15; j++){
-                        s1[i][j] = (float)(Math.asin((float)2/3 * 0.2 * (float)(x[0]+2.5)/17));
+                        s1[i][j] = f1(x[j]);
                     }
                     break;
                 case 9, 11, 12, 14, 15, 16, 18:
                     for (int j = 0; j<15; j++){
-                        s1[i][j] = (float)(Math.pow(Math.asin((float)(x[j]+2.5)/17), (float)1/3) * ((float)1/2 - Math.pow(Math.tan(x[j]), (float)1/3)));
+                        s1[i][j] = f2(x[j]);
                     }
                     break;
                 case 6, 7, 8, 10, 13, 19:
                     for (int j = 0; j<15; j++){
-                        s1[i][j] = (float)(Math.log(Math.pow(Math.sin(Math.pow(Math.E, (float)x[j]/9)), 2)));
+                        s1[i][j] = f3(x[j]);
                     }
                     break;
             }
         }
         for (int i =0; i<14; i++){
             for (int j=0; j<15; j++) {
-                System.out.print((float)Math.round(s1[i][j]*10000)/10000 + ", ");
+                System.out.print(String.format("%,.4f", s1[i][j]) + ", ");
             }
             System.out.println();
         }
+    }
+    public static float f1(float x){
+        return (float)(Math.asin((float)2/3 * 0.2 * (float)(x+2.5)/17));
+    }
+    public static float f2(float x){
+        return (float)(Math.pow(Math.asin((float)(x+2.5)/17), (float)1/3) * ((float)1/2 - Math.pow(Math.tan(x), (float)1/3)));
+    }
+    public static float f3(float x){
+        return (float)(Math.log(Math.pow(Math.sin(Math.pow(Math.E, (float)x/9)), 2)));
     }
 }
